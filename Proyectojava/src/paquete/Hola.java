63baa79dab55df3ac92400520e579e87c774e6d5
gabrielAlber <104063514+gabrielAlber@users.noreply.github.com>
2022-04-20 13:09:53 +0200

    Update Hola.java

--- Proyectojava/src/paquete/Hola.java
+++ Proyectojava/src/paquete/Hola.java
@@ -4,5 +4,6 @@ public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Holamundo");
 		System.out.println("Qué tal?");
+		System.out.println("Me has escuchado?");
 	}
 }
